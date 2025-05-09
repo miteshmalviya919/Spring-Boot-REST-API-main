b9d41a499f6032fe7573f189f96407e7758e1b80
Mitesh Malvia <miteshmalvia919@gmail.com>
2025-05-10 00:06:56 +0530

    myfirst commit

---
+++ src/main/java/com/mitesh/rest/api/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.mitesh.rest.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mitesh.rest.api.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
